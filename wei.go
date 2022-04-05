ed10939b60e1e4cbeca62ff24112188c81b78d00
yakud <yakudgm@gmail.com>
2022-04-05 15:59:22 +0200

    Rollback wei converter due another bugs

--- wei.go
+++ wei.go
@@ -4,11 +4,10 @@ import (
 	"math/big"
 )
 
-// ConvertToWei deprecated. Instead of ConvertToWei just use decimal.ToBig()
 func ConvertToWei(dec *Decimal, mantissa uint8) *big.Int {
 	if dec == nil || dec.value == nil {
 		return new(big.Int).SetUint64(0)
 	}
 
-	return NewDecimal(dec).ToBig()
+	return NewDecimal(dec).Rescale(mantissa).ToBig()
 }
